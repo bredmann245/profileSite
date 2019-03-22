385dab42adf410580edf49643a60a8e14e47c07d
Brandon <redmannbw@slu.edu>
2019-03-21 20:40:19 -0500

    2nd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom/BrowserRouter";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
@@ -8,13 +8,23 @@ import Contact from "./pages/Contact.jsx";
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </div>
-      </Router>
+      <div>
+        <Route
+          exact={true}
+          path={process.env.PUBLIC_URL + "/profile"}
+          component={Home}
+        />
+        <Route
+          exact={true}
+          path={process.env.PUBLIC_URL + "/about"}
+          component={About}
+        />
+        <Route
+          exact={true}
+          path={process.env.PUBLIC_URL + "/contact"}
+          component={Contact}
+        />
+      </div>
     );
   }
 }
